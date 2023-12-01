766dab311f52c960d7f83a2ac3d0ab8b18d9296a
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-01 12:27:24 -0700

    gets first and second number using an array, prints each array

--- trebuchet.py
+++ trebuchet.py
@@ -7,3 +7,17 @@ for line in input:
 
 print(arrLine)
 input.close()
+
+for item in arrLine:
+    firstNum = 0
+    secondNum = 0
+    localResult = []
+    for char in item:
+        if char.isnumeric():
+            firstNum = char
+    for char in reversed(item):
+        if char.isnumeric():
+            secondNum = char
+    localResult.append(secondNum)
+    localResult.append(firstNum)
+    print(localResult)
