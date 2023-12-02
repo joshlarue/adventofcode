47cb4935132bc07d2c5ad2b1403b14c9235918f2
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-02 11:03:50 -0700

    chunked array items into 3, 4, and 5

---
+++ day1/trebuchetwords.py
@@ -0,0 +1,39 @@
+input = open("trebuchetinput.txt", "rt")
+arrLine = []
+result = 0
+numbers = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+
+for line in input:
+    line = line.rstrip()
+    arrLine.append(line)
+input.close()
+
+# for item in arrline
+#   loop through the array forwards in 3, 4, and 5 sized chunks
+#       for number in numbers
+#           if number in chunk3
+#               firstNum = numbers[number].value()? idk
+
+for item in arrLine:
+    firstNum = 0
+    secondNum = 0
+    localResult = ''
+    for item in arrLine:
+        i = 0
+        for i in range(0, len(item)):
+            threeChunk = item[i:3] # what happens if this goes beyond the end of the string?
+            fourChunk = item[i:4]
+            fiveChunk = item[i:5]
+            if threeChunk in numbers:
+                print(threeChunk)
+            if fourChunk in numbers:
+                print(fourChunk)
+            if fiveChunk in numbers:
+                print(fiveChunk)
+    
+
+    #localResult += secondNum
+    #localResult += firstNum
+    #result += int(localResult)
+
+#print(result)
