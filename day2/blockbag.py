3caa22f435ea2b97cc3e04be092c11ea5cb3a919
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-02 13:00:21 -0700

    moved local colour counters outside of color loop

--- day2/blockbag.py
+++ day2/blockbag.py
@@ -1,7 +1,10 @@
 numRed = 12
 numGreen = 13
 numBlue = 14
-input = open("blockbag.txt", "rt")
+red = 0
+green = 0
+blue = 0
+input = open("day2/blockbag.txt", "rt")
 
 lineNum = 1
 sumID = 0
@@ -14,10 +17,10 @@ for line in input:
     for item in gameArr:
         setArr = item.split(",") # splits the game up into each set of blocks
         print(setArr)
+        localRed = '' # using strings so that I can easily push multiple consecutive digits
+        localGreen = ''
+        localBlue = ''
         for color in setArr:
-            localRed = '' # using strings so that I can easily push multiple consecutive digits
-            localGreen = ''
-            localBlue = ''
             if 'red' in color:
                 for char in color:
                     if char.isdigit(): localRed += char
@@ -30,9 +33,9 @@ for line in input:
                 for char in color:
                     if char.isdigit(): localBlue += char
                 print(localBlue)
-            if localRed == '': localRed = 0 # if there was no color of the specified type, set to 0, otherwise this causes typeerrors
-            if localGreen == '': localGreen = 0
-            if localBlue == '': localBlue = 0
-            if (int(localRed) < numRed) and (int(localGreen) < numGreen) and (int(localBlue) < numBlue):
-                sumID += gameID
+    if localRed == '': localRed = 0 # if there was no color of the specified type, set to 0, otherwise this causes typeerrors
+    if localGreen == '': localGreen = 0
+    if localBlue == '': localBlue = 0
+    if (int(localRed) <= numRed) and (int(localGreen) <= numGreen) and (int(localBlue) <= numBlue):
+        sumID += gameID
 print(sumID)
