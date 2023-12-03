e7bfd2cd8dbb6938d484566e4ec57bd0faa05587
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-02 18:10:45 -0700

    created arrays for each color

---
+++ day2/cubeconundrum.py
@@ -0,0 +1,38 @@
+input = open("day2/test.txt", "rt")
+lineNum = 1
+sumID = 0
+
+for line in input:
+    gameID = lineNum
+    line = line.replace(":", ";")
+    gameArr = line.split(";")
+    numRed = []
+    numGreen = []
+    numBlue = []
+    print(line)
+    gameArr.pop(0) # gets rid of the game label so that gameArr is just an array of each game's colors
+    for item in gameArr:
+        setArr = item.split(",") # splits the game up into each set of blocks
+        localRed = '' # using strings so that I can easily push multiple consecutive digits
+        localGreen = ''
+        localBlue = ''
+        for color in setArr:
+            if 'red' in color:
+                for char in color:
+                    if char.isdigit(): localRed += char
+            if 'green' in color:
+                for char in color:
+                    if char.isdigit(): localGreen += char
+            if 'blue' in color:
+                for char in color:
+                    if char.isdigit(): localBlue += char
+        if localRed == '': localRed = 0 # if there was no color of the specified type, set to 0, otherwise this causes typeerrors
+        if localGreen == '': localGreen = 0
+        if localBlue == '': localBlue = 0
+        # create array of number of colors for each set and loop through to find lowest number
+        # will have to be array of just that color
+        numRed.append(int(localRed))
+        numGreen.append(int(localGreen))
+        numBlue.append(int(localBlue))
+    print(numRed, numGreen, numBlue)
+    lineNum += 1
