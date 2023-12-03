d935f39e9820ec697c4c36d2c666f402ac4c2b08
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-02 22:37:01 -0700

    functionized and adding check if beside logic

--- day3/gearratios.py
+++ day3/gearratios.py
@@ -1,7 +1,28 @@
 SPECIAL_CHARS = '!@#$%^&*?/=-_~`\{}[]()<>'
 # or use if not .isalnum()?
-input = open("text.txt", "rt")
+input = open("day3/test.txt", "rt")
 
-def checkIfBeside():
+def getLineNumList():
+    nums = ''
     # if width of number +1 or -1 = special char then add
+    for line in input:
+        i = 0
+        for i in range(0, len(line)):
+            if line[i].isdigit() or line[i] in SPECIAL_CHARS:
+                nums += line[i]
+            if line[i] == '.' and line[i-1].isdigit():
+                nums += ' '
+        checkIfBeside(nums)
 
+def checkIfBeside(list):
+    i = 0
+    for i in range(0, len(list)):
+        if (list[i].isdigit() and list[i-1] in SPECIAL_CHARS) or (list[i].isdigit() and list[i+1] in SPECIAL_CHARS):
+            pass
+
+
+
+getLineNumList()
+checkIfBeside()
+
+input.close()
