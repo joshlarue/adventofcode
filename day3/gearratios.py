ea2885db9e423e2aa51a6545b21f3c00a0146fc4
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-03 20:11:06 -0700

    added range checking

--- day3/gearratios.py
+++ day3/gearratios.py
@@ -15,9 +15,11 @@ for i in range(0, len(twoD[0])):
     for j in range(0, len(twoD)):
         # this could probably be done better
         # keep getting index out of range error when i=9
-        if (twoD[i][j].isdigit() and twoD[i-1][j] in SPECIAL_CHARS) or (twoD[i][j].isdigit() and twoD[i+1][j-1] in SPECIAL_CHARS):
-            print("add!")
-        #j+=1
-    #i+=1
+        if i > 0 or j > 0 or i < 9 or j < 9:
+            if (twoD[i][j].isdigit() and twoD[i+1][j] in SPECIAL_CHARS) or (twoD[i][j].isdigit() and twoD[i][j+1] in SPECIAL_CHARS) or (twoD[i][j].isdigit() and twoD[i+1][j+1] in SPECIAL_CHARS):
+                print("add!")
+        elif i == 0 or j == 0 or i == 9 or j == 9:
+            pass
+            #if (twoD[i][j].isdigit() and twoD[])
 
 input.close()
