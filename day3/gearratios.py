90b18d701aa2f0a798f303da73847eb6a45d3473
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-03 22:17:57 -0700

    added loop logic for each character instead of hardcoding

--- day3/gearratios.py
+++ day3/gearratios.py
@@ -11,15 +11,13 @@ print(twoD)
 
 i = 0
 j = 0
-for i in range(0, len(twoD[0])):
-    for j in range(0, len(twoD)):
+for i in range(0, len(twoD)):
+    for j in range(0, len(twoD[0])):
         # this could probably be done better
         # keep getting index out of range error when i=9
-        if i > 0 or j > 0 or i < 9 or j < 9:
-            if (twoD[i][j].isdigit() and twoD[i+1][j] in SPECIAL_CHARS) or (twoD[i][j].isdigit() and twoD[i][j+1] in SPECIAL_CHARS) or (twoD[i][j].isdigit() and twoD[i+1][j+1] in SPECIAL_CHARS):
-                print("add!")
-        elif i == 0 or j == 0 or i == 9 or j == 9:
-            pass
-            #if (twoD[i][j].isdigit() and twoD[])
+        if twoD[i][j].isdigit() and any(twoD[x][y] in SPECIAL_CHARS
+                                        for x in range(max(0, i-1), min(len(twoD), i+2))
+                                        for y in range(max(0, j-1), min(len(twoD[0]), j+2))):
+            print("add!")
 
 input.close()
