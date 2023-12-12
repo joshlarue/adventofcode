6f7214b7c6c0201394ca684320dafb3c7d3b40b2
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-11 22:45:15 -0700

    added coordinates, numrows, numcols, and attempted to obtain columns with no#s

--- day11/space.py
+++ day11/space.py
@@ -1,18 +1,52 @@
-input = open('day11/test.txt', 'rt')
-numRows = 0
-numColumns = 0
-coordinates = []
-
-for i, row in enumerate(input):
-    numRows += 1
-    row = row.rstrip()
-    for j, column in enumerate(row):
-        if i == 1:
+def getSize():
+    input = open('day11/test.txt', 'rt')
+    numRows = 0
+    numColumns = 0
+    for line in input:
+        numRows += 1
+        for numColumns in range(0, len(line)):
             numColumns += 1
-        if column in '#':
-            coordinates.append((i, j))
+    return numRows, numColumns
 
 
-print(coordinates)
-print(numRows)
-print(numColumns)
+
+
+def main():
+    input = open('day11/test.txt', 'rt')
+    coordinates = []
+    lineYN = []
+    numRows, numColumns = getSize()
+    for i, row in enumerate(input):
+        row = row.rstrip()
+        prevSpace = True
+        
+        for j, column in enumerate(row):
+            if column == '.':
+                for c in range(0, 9):
+                    if row[c] == '.' and prevSpace == True:
+                        prevSpace = True
+                    else:
+                        prevSpace = False
+            if column in '#':
+                coordinates.append((i, j))
+                
+        if prevSpace == True:
+            lineYN.append(i)
+            
+
+
+
+
+
+
+
+
+
+
+    print(coordinates)
+    print(numRows)
+    print(numColumns)
+    print(lineYN)
+
+if __name__ == '__main__':
+    main()
