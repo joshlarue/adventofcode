f86a2c0d2b62d1fb552fadb7630d23b87ac871a2
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-11 21:39:13 -0700

    added coordinates

--- day11/space.py
+++ day11/space.py
@@ -1,14 +1,18 @@
 input = open('day11/test.txt', 'rt')
 numRows = 0
 numColumns = 0
+coordinates = []
+
 for i, row in enumerate(input):
     numRows += 1
     row = row.rstrip()
-    for column in row:
+    for j, column in enumerate(row):
         if i == 1:
             numColumns += 1
+        if column in '#':
+            coordinates.append((i, j))
 
 
-
+print(coordinates)
 print(numRows)
 print(numColumns)
