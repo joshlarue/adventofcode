aa4e8d1a53bc401ca51c1df8dd006d6d9253a917
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-11 23:07:08 -0700

    got column detection working properly

--- day11/space.py
+++ day11/space.py
@@ -15,38 +15,38 @@ def main():
     input = open('day11/test.txt', 'rt')
     coordinates = []
     lineYN = []
+    rows = []
     numRows, numColumns = getSize()
-    for i, row in enumerate(input):
+    
+    input = open('day11/test.txt', 'rt')
+    for row in input:
         row = row.rstrip()
+        rows.append(row)
+
+    for i, item in enumerate(rows):
         prevSpace = True
-        
-        for j, column in enumerate(row):
-            if column == '.':
-                for c in range(0, 9):
-                    if row[c] == '.' and prevSpace == True:
-                        prevSpace = True
-                    else:
-                        prevSpace = False
-            if column in '#':
+
+        for j, char in enumerate(item):
+            if char in '#':
                 coordinates.append((i, j))
-                
+
+        if item[i] == '.':
+            for n, blank in enumerate(rows):
+                if rows[n][i] == '.' and prevSpace == True:
+                    prevSpace = True
+                else:
+                    prevSpace = False
         if prevSpace == True:
             lineYN.append(i)
-            
-
-
-
-
-
-
-
-
-
+                    
+    input.close()
 
     print(coordinates)
-    print(numRows)
-    print(numColumns)
     print(lineYN)
+
+
+
+
 
 if __name__ == '__main__':
     main()
