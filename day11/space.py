bedf565d18aa0b03b033eefb2fbae7c8fe4d49c1
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-11 23:52:29 -0700

    added insert column functionality. does not work yet

--- day11/space.py
+++ day11/space.py
@@ -25,10 +25,21 @@ def getGalaxiesAndOpenColumns(rows, coordinates, openColumns):
         if prevSpace == True:
             openColumns.append(i)
 
+def insertColumns(rows, openColumns, numColumns):
+    for i in range(0, len(openColumns)):
+        for j, row in enumerate(rows):
+            colToInsert = rows[j]
+            newCol = colToInsert[:openColumns[i]+2] + '*' + colToInsert[openColumns[i]+2:]
+            rows[j] = newCol
+
 def getOpenRows(rows, openRows):
     for i, row in enumerate(rows):
         if not '#' in row:
             openRows.append(i)
+    
+def insertRows(rows, openRows, numColumns):
+    for i in range(0, len(openRows)):
+        rows.insert(openRows[i]+1, '*'*numColumns)
 
 def main():
     input = open('day11/test.txt', 'rt')
@@ -45,11 +56,15 @@ def main():
 
     getGalaxiesAndOpenColumns(rows, coordinates, openColumns)
     getOpenRows(rows, openRows)
+    insertRows(rows, openRows, numColumns)
+    insertColumns(rows, openColumns, numColumns)
     input.close()
 
     print(coordinates)
     print(openColumns)
     print(openRows)
+    for row in rows:
+        print(row)
 
 
 
